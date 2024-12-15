6c2604c137f522b1f28da3df50de92bf7a345f19
Z1xton <zheniaburlaenko@gmail.com>
2024-12-15 12:31:04 +0200

    Add files via upload

---
+++ scirpttt.js
@@ -0,0 +1,5 @@
+function myFunction(string) {
+    let message = "Hello World!";
+    document.getElementById('par1').innerHTML = message;
+    document.getElementById('par2').innerHTML = string;
+}
